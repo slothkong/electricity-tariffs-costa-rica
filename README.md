953a7379a5bc403a7c6ce96e3d2b45560dd914cc
Mario Loaiciga <jorge.mlr.elec@gmail.com>
2026-06-28 20:33:47 -0600

    feat: Data cleaning procedure (#1)
    * Add sample configuration
    
    NOTE: Includes only worksheets with 2013 data
    
    * Provision directory for data staging
    
    * Add Excel conversion procedure
    
    * Document motivation and usage
    
    * List python requirements
    
    * Move month mapping from config to global variables
    
    * Correct defaults for local file paths
    
    * Improve motivation and usage remarks
    
    * Fix month mapping usage
    
    * Configure cell ranges for all residential tariffs 2013~2026
    
    * Describe configuration file schema
    
    * Rename project and document features
    
    * Review config for residential tariffs
    
    * Correct year-month formatting
    
    * Avoid reading in formulas
    
    * Fix deprecation warns about casting all-nan cols
    
    * Replace inferred zeros with nulls
    
    NOTE:  openpyxl tends to infer zeros for cell numeric cells left empty on the workbook. We force those to null, and assume there will never be tariffs  of price 0.0
    
    * Strip tariff block values
    
    * Drop rows without valid metric value
    
    * Include year and month as standalone dimensions
    
    * Document additional dimension

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# electricity-tariffs-costa-rica
-Data clean room for ARASEP historical records on Costa Rica electricity pricing
+# Costa Rica's Electricity Tariffs
+Costa Rica's ["Authoridad Reguladora de los Servicios Publicos" (ARASEP)](https://aresep.go.cr/) publishes historical records and charts on electricity pricing.
+Although while said data is of relative good quality, it is somewhat hard to work with, when using open-source analytics tooling. Some of the main hurdles are:
+* **Proprietary format**: Published as an Excel workbook
+* **Non-standard structure**: Data from each distribution company spreads across multiple worksheets. Futhermore, tariff data is framed within tables of variable column count (i.e. months as columns/metrics)
+* **Buggy and/or misleading visualization**: As of June 2026, the PowerBI visualizations embedded on the [ARASEP's website](https://aresep.go.cr/electricidad/tarifas/) does not properly reflect average tariff prices per distribution company or across years
+
+Tooling in this repository contains is used to create/update the ["Costa Rica's Electricity Tariffs" Kaggle Dataset](https://www.kaggle.com/datasets/slothkong/electricity-tariffs-costa-rica), which aims at addressing the aforementioned challenges.
+
+## Features 
+
+Enables convertiion of ARESEP's published worksheets into a more convenient data structures, essentially going from multiple tales of variable width:
+
+```
+T-RE              Enero  Febrero  Marzo  Abril  Mayo  Junio  Julio  Agosto  Septiembre  Octrubre  Noviembre  Deciembre   
+Primeros 200 KWh  72     72       72     72     72    78     75     75      75          65        63         63 
+kWh adicional     93     93       93     101    101   101    97     97      97          84        82         82
+```
+
+to single standard wide table of fix width (i.e. 6 dimensions and 1 metric):
+```
+annio_mes  annio  mes  distribuidor  tipo_de_tarifa  bloque_de_tarifa  colones_por_kwh
+2013-01    2013   01   CAR           T-RE            Primeros 200 KWh  72
+2013-01    2013   01   CAR           T-RE            kWh adicional     93
+...        ...    ...  ...           ...             ...               ...
+2013-12    2013   12   CAR           T-RE            Primeros 200 KWh  63
+2013-12    2013   12   CAR           T-RE            kWh adicional     82
+```
+
+## Usage
+
+Install the required `python` libraries:
+```bash
+pip install -r requirements.txt
+```
+
+Optionally, override the local paths for input/output files:
+```bash
+export WORKBOOK_PATH="./kaggle/input/datasets/slothkong/electricity-tariffs-costa-rica/Cuadro E-8 Tarifas electricas final.xlsx"
+export CONFIG_PATH="./kaggle/working/cfgs.yaml"
+export DATAFRAME_PATH="./kaggle/working/electricity-tariffs-costa-rica.csv"
+```
+
+Run the main data processing script:
+```bash
+python main.py
+```
+
+## Configuration
+
+To properly parse the contents of Excel workbooks, this tooling relias on user-provider configuration about which cell ranges are processed.
+The configuration follows the schemae:
+```yaml
+worksheets:
+  <name of a worksheet>:                   # Example: "CNFL 2023"
+    ranges:
+      <name above an specific cell range>: # Example: "T-RE" (i.e. acronym of residential tariff)
+        range: <specifc cell range>        # Example: "A9:M11"
+```
+Checkout the [cfgs.yaml](./kaggle/working/cfgs.yaml) file to see the complete configuration.
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,136 @@
+import yaml
+import pandas
+import numpy
+
+import os
+import requests
+
+from pandas import DataFrame
+from numpy import array
+
+from openpyxl import load_workbook, Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+from openpyxl.cell.cell import Cell
+
+
+WORKBOOK_URL = os.getenv("WORKBOOK_URL", "https://aresep-my.sharepoint.com/:x:/g/personal/multimedia_aresep_go_cr/ET6L4k-QyphAgLpEwSYeNegBbLvOGM7mF0n2vZxId_SGeQ?e=YGhFZr&download=1")
+WORKBOOK_PATH =  os.getenv("WORKBOOK_PATH", "./kaggle/input/datasets/slothkong/tarifas-electricas-de-costa-rica/Cuadro E-8 Tarifas electricas final.xlsx")
+CONFIG_PATH = os.getenv("CONFIG_PATH", "./kaggle/working/cfgs.yaml")
+DATAFRAME_PATH = os.getenv("DATAFRANE_PATH", "./kaggle/working/electricity-tariffs-costa-rica.csv")
+MONTH_MAPPING = {"enero": "01", "febrero": "02", "marzo": "03", "abril": "04", "mayo": "05", "junio": "06", "julio": "07", "agosto": "08", "septiembre": "09", "octubre": "10", "noviembre": "11", "diciembre": "12"}
+
+def download_workbook(workbook_url: str, workbook_path: str) -> None:
+    
+    response = requests.get(workbook_url, stream=True)
+    response.raise_for_status()
+
+    with open(workbook_path, "wb") as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
+def select_worksheet(worksheet_title: str, workbook: Workbook) -> Worksheet:
+
+    def worksheet_lookup(workbook: Workbook)-> dict[str, int]:
+        worksheet_lookup_map = {}
+
+        for idx, worksheet in enumerate(workbook.worksheets):
+            worksheet_lookup_map.update({worksheet.title: idx})
+
+        return worksheet_lookup_map
+    
+    idx = worksheet_lookup(workbook).get(worksheet_title)
+
+    if not idx:
+        raise ValueError(f"Desired worksheet title '{worksheet_title}' not in workbook.")
+
+    return workbook.worksheets[idx]
+
+def pivot_table(cell_range: tuple[Cell,])-> DataFrame:
+
+    column_names = ["bloque_de_tarifa"] + [month for month in MONTH_MAPPING.keys()]
+    column_types_mapping = dict({column_names[0]: "str"}) | dict(zip(column_names[1:], ["float64"] * len(column_names[1:])))
+
+    for row_idx in range(len(cell_range)):
+        values = []
+        for row in cell_range[row_idx]:
+    
+            values.append([row.value])
+    
+        record = DataFrame.from_records(
+            data=numpy.array(values).T,
+            columns=column_names
+        )
+
+        if record.empty:
+            raise ValueError("Unable to extract valid data from user-defined cell range!")
+
+        record = record.astype(column_types_mapping)
+        
+        if row_idx == 0:
+            dataframe = record
+        else:
+            dataframe = pandas.concat([dataframe, record], ignore_index=True)
+    
+    dataframe = pandas.melt(dataframe,
+                            id_vars=[column_names[0]],
+                            value_vars=column_names[1:],
+                            var_name="mes",
+                            value_name="colones_por_kwh")
+
+    dataframe = dataframe.replace(0, numpy.nan)
+    dataframe[column_names[0]] = dataframe[column_names[0]].str.strip()
+
+    return dataframe
+
+def convert_workbook_to_dataframe(workbook_path: str, config_path: str) -> DataFrame:
+
+    column_names = ["annio_mes", "annio", "mes", "distribuidor", "tipo_de_tarifa", "bloque_de_tarifa", "colones_por_kwh"]
+    is_initial_loop = True
+
+    with open(config_path, "r") as fp:
+        cfgs = yaml.safe_load(fp)
+
+    workbook = load_workbook(workbook_path, data_only=True)
+    for worksheet_title, worksheets_cfg in cfgs["worksheets"].items():
+
+        worksheet = select_worksheet(worksheet_title, workbook)
+        for tariff_type, worksheet_cfg in worksheets_cfg["ranges"].items():
+            
+            cell_range = worksheet_cfg["range"]
+            print(f"Processing range '{cell_range}' from worksheet '{worksheet_title}'...")
+
+            year = worksheet_title.split(" ")[1]
+            distributor_acronym = worksheet_title.split(" ")[0]
+
+            tmp_dataframe = pivot_table(worksheet[cell_range])
+            tmp_dataframe["annio"] = year
+            tmp_dataframe["tipo_de_tarifa"] = tariff_type
+            tmp_dataframe["distribuidor"] = distributor_acronym
+            tmp_dataframe["annio_mes"] = tmp_dataframe["mes"].apply(lambda x: f"{year}-{MONTH_MAPPING.get(x)}")
+            tmp_dataframe["mes"] = tmp_dataframe["mes"].apply(lambda x: f"{MONTH_MAPPING.get(x)}")
+            tmp_dataframe = tmp_dataframe[column_names]
+
+            if is_initial_loop:
+                dataframe = tmp_dataframe
+                is_initial_loop = False
+            else:
+                dataframe = pandas.concat([dataframe, tmp_dataframe], ignore_index=True)
+
+    dataframe.dropna(subset=["colones_por_kwh"], inplace=True)
+    dataframe.sort_values(by=column_names[0:-1], inplace=True)
+    return dataframe
+
+def main() -> None:
+    
+    download_workbook(WORKBOOK_URL, WORKBOOK_PATH)
+    print(f"Successfully downloaded input workbook from '{WORKBOOK_URL}'")
+    
+    dataframe = convert_workbook_to_dataframe(WORKBOOK_PATH, CONFIG_PATH)
+    
+    dataframe.to_csv(DATAFRAME_PATH, index=False)
+    print(f"Successfully wrote dataframe to '{DATAFRAME_PATH}'")
+
+
+
+if __name__ == "__main__":
+    main()
